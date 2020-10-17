acf023945fe0a670c40b698e60af7ef01ccba635
gennadytabala <gennady.tabala@gmail.com>
2020-10-17 17:13:38 +0300

    composing components, placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 
-const continents = ['Am', 'Af', 'An','Eu', 'Au', 'As'];
+class IssueFilter extends React.Component {
+	render() {
+		return (
+			<div>This is a placeholder for the issue filter.</div>
+		);
+	}
+}
 
-const helloContinents = Array.from(continents, c => `Hello, ${c}!`);
-const message = helloContinents.join(' ');
+class IssueTable extends React.Component {
+	render() {
+		return (
+			<div>This is a placeholder for the issue table.</div>
+		);
+	}
+}
 
-const element = (
-	<div title = "Outer div">
-		<h1>{message}</h1>
-	</div>
-);
+class IssueAdd extends React.Component {
+	render() {
+		return (
+			<div>This is a placeholder for the issue Add.</div>
+		);
+	}
+}
+
+class IssueList extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Issue Tracker</h1>
+				<IssueFilter />
+				<hr />
+				<IssueTable />
+				<hr />
+				<IssueAdd />
+			</React.Fragment>
+		);
+	}
+}
+
+const element  = <IssueList />;
 
 ReactDOM.render(element, document.getElementById('content'));
