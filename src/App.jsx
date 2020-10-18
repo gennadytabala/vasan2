abc47e4c293798959b9558a6c06054e6728a3af7
gennadytabala <gennady.tabala@gmail.com>
2020-10-18 21:53:12 +0300

    source of issues is array now

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
 
+const issues = [
+	{
+		id:1,
+		status:'New',
+		owner: 'Ravan',
+		effort: '5',
+		created: new Date('2018-05-16'),
+		due: undefined,
+		title: 'Error in console when clicking Add'
+	},
+	{
+		id:2,
+		status:'Assigned',
+		owner: 'Eddie',
+		effort: '14',
+		created: new Date('2018-08-03'),
+		due: new Date('2018-08-22'),
+		title: 'Missing bottom border on panel'
+	}
+];
+
 class IssueFilter extends React.Component {
 	render() {
 		return (
@@ -9,8 +30,43 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
 	render() {
+		const issueRows = issues.map(
+			issue => <IssueRow key = {issue.id} issue = {issue}>{issue.title}</IssueRow>
+			);
 		return (
-			<div>This is a placeholder for the issue table.</div>
+			<table className = "bordered-table">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Status</th>
+						<th>Owner</th>
+						<th>Created</th>
+						<th>Effort</th>
+						<th>DueDate</th>
+						<th>Title</th>
+					</tr>
+				</thead>
+				<tbody>
+					{issueRows}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+class IssueRow extends React.Component {
+	render() {
+		const issue = this.props.issue;
+		return (
+			<tr>
+				<td >{issue.id}</td>
+				<td >{issue.status}</td>
+				<td >{issue.owner}</td>
+				<td >{issue.created.toDateString()}</td>
+				<td >{issue.effort}</td>
+				<td >{issue.due ? issue.due.toDateString(): ''}</td>
+				<td >{issue.title}</td>
+			</tr>
 		);
 	}
 }
@@ -38,6 +94,6 @@ class IssueList extends React.Component {
 	}
 }
 
-const element  = <IssueList />;
+const element = <IssueList />;
 
 ReactDOM.render(element, document.getElementById('content'));
