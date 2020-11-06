48b95464ccfb94aae2e49dad9b9e95b1c498b02a
htabala <htabala@universalna.com>
2020-11-06 19:50:45 +0200

    graphql schema in separated file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,34 @@
+const fs = require('fs');
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage
+    },
+};
+
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+    resolvers,
+});
 
 const app = express();
+
 app.use(express.static('public'));
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(3000, function() {
     console.log('App started on 3000');
