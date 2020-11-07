436fc0385c351174a39bc683a876680a26c64bba
gennadytabala <gennady.tabala@gmail.com>
2020-11-08 00:35:24 +0200

    list api

--- server/server.js
+++ server/server.js
@@ -4,19 +4,44 @@ const { ApolloServer } = require('apollo-server-express');
 
 let aboutMessage = "Issue Tracker API v1.0";
 
+const issuesDB = [
+	{
+		id: 1,
+		status: 'New',
+		owner: 'Ravan',
+		effort: '5',
+		created: new Date('2018-05-16'),
+		due: undefined,
+		title: 'Error in console when clicking Add'
+	},
+	{
+		id: 2,
+		status: 'Assigned',
+		owner: 'Eddie',
+		effort: '14',
+		created: new Date('2018-08-03'),
+		due: new Date('2018-08-22'),
+		title: 'Missing bottom border on panel'
+	}
+
+];
 
 const resolvers = {
     Query: {
-        about: () => aboutMessage,
+		about: () => aboutMessage,
+		issueList
     },
     Mutation: {
         setAboutMessage
     },
 };
 
-
 function setAboutMessage(_, { message }) {
     return aboutMessage = message;
+}
+
+function issueList(){
+	return issuesDB;
 }
 
 const server = new ApolloServer({
