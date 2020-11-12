5dc2c9a1010b68257b481663cbcd3a86473f690d
Табала Геннадій Віталійович <HTabala@universalna.com>
2020-11-12 15:15:04 +0200

    addIssue api

--- server/server.js
+++ server/server.js
@@ -2,46 +2,75 @@ const fs = require('fs');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
+const { GraphQLScalarType } = require('graphql');
+const { Kind, parseValue } = require('graphql/language');
+
+
 let aboutMessage = "Issue Tracker API v1.0";
 
-const issuesDB = [
-	{
-		id: 1,
-		status: 'New',
-		owner: 'Ravan',
-		effort: '5',
-		created: new Date('2018-05-16'),
-		due: undefined,
-		title: 'Error in console when clicking Add'
-	},
-	{
-		id: 2,
-		status: 'Assigned',
-		owner: 'Eddie',
-		effort: '14',
-		created: new Date('2018-08-03'),
-		due: new Date('2018-08-22'),
-		title: 'Missing bottom border on panel'
-	}
+const issuesDB = [{
+        id: 1,
+        status: 'New',
+        owner: 'Ravan',
+        effort: '5',
+        created: new Date('2018-05-16'),
+        due: undefined,
+        title: 'Error in console when clicking Add'
+    },
+    {
+        id: 2,
+        status: 'Assigned',
+        owner: 'Eddie',
+        effort: '14',
+        created: new Date('2018-08-03'),
+        due: new Date('2018-08-22'),
+        title: 'Missing bottom border on panel'
+    }
 
 ];
 
+const GraphQLDate = new GraphQLScalarType({
+    name: 'GraphQLDate',
+    description: 'A date() type in GraphQL as a scalar',
+    serialize(value) {
+        return value.toISOString();
+    },
+    parseValue(value) {
+        return new Date(value);
+    },
+    parseLiteral(ast) {
+        return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined;
+    }
+});
+
 const resolvers = {
     Query: {
-		about: () => aboutMessage,
-		issueList
+        about: () => aboutMessage,
+        issueList
     },
     Mutation: {
-        setAboutMessage
+        setAboutMessage,
+        issueAdd
     },
+    GraphQLDate
 };
+
+function issueAdd(_, { issue }) {
+
+    issue.created = new Date();
+    issue.id = issuesDB.length + 1;
+    if (issue.status == undefined) issue.status = 'New';
+    issuesDB.push(issue);
+    return issue;
+
+}
 
 function setAboutMessage(_, { message }) {
     return aboutMessage = message;
 }
 
-function issueList(){
-	return issuesDB;
+function issueList() {
+    return issuesDB;
 }
 
 const server = new ApolloServer({
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
+	if (dateRegex.test(value)) return new Date(value);
+	return value;
+}
+
 class IssueFilter extends React.Component {
 	render() {
 		return (
@@ -38,9 +45,9 @@ function IssueRow(props) {
 			<td >{issue.id}</td>
 			<td >{issue.status}</td>
 			<td >{issue.owner}</td>
-			<td >{issue.created}</td>
+			<td >{issue.created.toDateString()}</td>
 			<td >{issue.effort}</td>
-			<td >{issue.due}</td>
+			<td >{issue.due ? issue.due.toDateString() : ''}</td>
 			<td >{issue.title}</td>
 		</tr>
 	);
@@ -110,7 +117,9 @@ class IssueList extends React.Component {
 			body: JSON.stringify({ query })
 		});
 
-		const result = await response.json();
+		const body = await response.text();
+		const result = JSON.parse(body, jsonDateReviver);
+
 		this.setState({ issues: result.data.issueList });
 
 	}
